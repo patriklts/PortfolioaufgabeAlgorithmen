f1059fc47334c555a656b9e07cb01a89dc3cfea0
BlackSeN1502 <sven.schmidt1502@gmail.com>
2023-05-08 12:20:36 +0200

    added nextIterator method to access somehow the element of a Set like an Array

--- src/main/TestDirections.java
+++ src/main/TestDirections.java
@@ -173,13 +173,14 @@ public class TestDirections {
 
 
 
-		//Test für getRoute(Airport src, Airport dst)					//TODO ganz komische Testmethode
+		//Test für getRoute(Airport src, Airport dst)					//TODO ganz komische Testmethode -> Man müsste jegliche Kombinationsmöglichkeit reinwerfen HOW?!!!!
 		System.out.println("\n---------------------------");
 		System.out.println("Test von der getRoute Methode mit eigenen Datensätzen:");
 		System.out.println("---------------------------");
-		for(Airport airport : ownDataAirportSet)
-			if(iteratorAirport.hasNext())
-				testGetRoute( ownDirections, ownDataAirportSet.iterator().next() , ownDataAirportSet.iterator().next() );
+		iteratorAirport = ownDataAirportSet.iterator();
+		iteratorAirport = nextIterator(iteratorAirport,2);
+		if( iteratorAirport.hasNext() && ownDataAirportSet.iterator().hasNext() )
+				testGetRoute( ownDirections, ownDataAirportSet.iterator().next() , iteratorAirport.next() );
 //				testGetRoute(d, new Airport("DME"), new Airport("TPE") );
 
 
@@ -329,6 +330,8 @@ public class TestDirections {
 
 	public static void testGetRoute(IDirections directions, Airport src, Airport dst){
 		System.out.println();
+		System.out.println("Airport Source: " + src);
+		System.out.println("Airport Destination: "+ dst);
 		System.out.println(directions.getRoute(src, dst));
 	}
 
@@ -356,12 +359,12 @@ public class TestDirections {
 
 		Set<Airport> airportSet = new LinkedHashSet<>();
 
-		airportSet.add( new Airport("FRA","Frankfurt") );
-		airportSet.add( new Airport("ZRH", "Zürich") );
-		airportSet.add( new Airport("CR","Crailsheim") );
-		airportSet.add( new Airport("SHA", "Schwäbisch Hall") );
-		airportSet.add( new Airport("MOS","Mosbach") );
-		airportSet.add( new Airport("S", "Stuttgart") );
+		airportSet.add( new Airport("FRA","Frankfurt") ); // 0
+		airportSet.add( new Airport("ZRH", "Zürich") ); // 1
+		airportSet.add( new Airport("CR","Crailsheim") ); // 2
+		airportSet.add( new Airport("SHA", "Schwäbisch Hall") ); // 3
+		airportSet.add( new Airport("MOS","Mosbach") ); // 4
+		airportSet.add( new Airport("S", "Stuttgart") ); // 5
 
 
 		return airportSet;
@@ -425,6 +428,23 @@ public class TestDirections {
 
 		return directSet;
 
+	}
+
+	/*
+	 * =========================
+	 *  Help Method Section
+	 * =========================
+	 */
+
+	public static Iterator<Airport> nextIterator(Iterator<Airport> iterator, int hops){
+		int counter = 0;
+
+		while( iterator.hasNext() && counter <= hops){
+			counter++;
+			iterator.next();
+		}
+
+		return iterator;
 	}
 
 
